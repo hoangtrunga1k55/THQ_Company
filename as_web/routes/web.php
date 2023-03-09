01ef33aad87ac827719abb15719f7308e994e9a5
phongtc <phongtc@its-global.vn>
2023-03-09 22:45:10 +0700

    Setup CI to check coding convention

--- as_web/routes/web.php
+++ as_web/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
